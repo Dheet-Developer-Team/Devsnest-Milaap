19caa7d1c6617cef5e8a5b54308ee83c817a540e
Sharique <mdsharique1205@gmail.com>
2021-10-09 18:58:42 +0530

    Login Page built

---
+++ src/components/RegisterCard.jsx
@@ -0,0 +1,26 @@
+export default function RegisterCard({ setLogin }) {
+  return (
+    <>
+      <h2 className="login-header">Register</h2>
+      Email
+      <input id="email" placeholder="Email"></input>
+      Password
+      <input id="password" placeholder="Password"></input>
+      Confirm Password
+      <input id="confirm-password" placeholder="Confirm Password"></input>
+      {/* <div className="RememberMe">
+        <input className="check-box" type="checkbox"></input> Remember Me
+      </div> */}
+      <button id="submit-btn">Submit</button>
+      Already a Member ?{" "}
+      <span
+        className="link"
+        onClick={() => {
+          setLogin(true);
+        }}
+      >
+        Login
+      </span>
+    </>
+  );
+}
